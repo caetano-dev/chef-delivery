44c3b47ef702657d73746d8b88242933e38b29cd
drull <pcaetanop@protonmail.com>
2023-10-19 16:56:42 -0300

    Finishing course

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -3,6 +3,7 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    @State private var currentIndex = 1
     let ordersMock: [OrderType] = [
         OrderType(id:1, name: "banner-burger", image: "barbecue-banner" ),
         OrderType(id:2, name: "banner prato feito", image: "brazilian-meal-banner" ),
@@ -10,11 +11,25 @@ struct CarouselTabView: View {
     ]
     
     var body: some View {
-        TabView{
+        TabView(selection: $currentIndex){
             ForEach(ordersMock){ mock in CarouselItemView(order: mock)
+                    .tag(mock.id)
                 
             }
-        }.frame(height: 180).tabViewStyle(.page(indexDisplayMode: .always))
+        }
+        .frame(height: 180)
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear{
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
+                _ in
+                withAnimation(.easeInOut(duration: 1.5)){
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    }
+                    currentIndex += 1
+                }
+            }
+        }
     }
 }
 
--- ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/HomeView/HomeView.swift
@@ -4,6 +4,9 @@ struct HomeView: View {
     
     @State private var isAnimating = false
     @State private var imageOffset: CGSize = .zero
+    @State private var buttonOffset: CGFloat = 0
+    @State private var showSecondScreen = false
+    let buttonHeight: CGFloat = 80
     
     
     var body: some View {
@@ -24,7 +27,7 @@ struct HomeView: View {
                     .opacity(isAnimating ? 0.5 : 0)
                 VStack{
                     Text("Chef Delivery")
-                        .font(.system(size: 40))
+                        .font(.system(size: 48))
                         .fontWeight(.heavy)
                         .foregroundStyle(Color.colorRed)
                         .opacity(isAnimating ? 1 : 0)
@@ -55,12 +58,80 @@ struct HomeView: View {
                                     }
                                 })
                         )
+                    ZStack{
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                        
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Descubra mais")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(Color("ColorRedDark"))
+                            .offset(x: 20)
+                        
+                        HStack{
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                                .frame(width: buttonOffset + buttonHeight)
+                            
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            ZStack{
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                Circle()
+                                    .fill(Color("ColorRedDark"))
+                                    .padding(8)
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24))
+                                    .bold()
+                                    .foregroundStyle(Color.white)
+                                
+                            }
+                            Spacer()
+                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if gesture.translation.width >= 0 && buttonOffset <= (geometry.size.width - 60) - buttonHeight {
+                                        withAnimation(.easeInOut(duration: 0.25)){
+                                            buttonOffset = gesture.translation.width
+                                        }
+                                        
+                                    }
+                            })
+                                .onEnded({ _ in
+                                    if buttonOffset > (geometry.size.width - 60) / 2 {
+                                        showSecondScreen = true
+                                        
+                                    } else {
+                                        withAnimation(.easeInOut(duration: 0.25)){
+                                            buttonOffset = 0
+                                        }
+                                        
+                                    }
+                                    
+                                })
+                        )
+                    }
+                    .frame(width: geometry.size.width - 60, height: buttonHeight)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(y: isAnimating ? 0 : 100)
                 }
                 .onAppear{
                     withAnimation(.easeInOut(duration: 1.5)){
                         isAnimating = true
                     }
                 }
+            }
+            .fullScreenCover(isPresented: $showSecondScreen){
+                ContentView()
             }
         }
         }
